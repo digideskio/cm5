33f447aca6e117b3c33cfe8e017fd47b16409ab1
Sque <sque@0x0lab.org>
2010-11-29 03:41:22 +0200

    Add: Licence Notices for GPLv3

--- config.inc.php
+++ config.inc.php
@@ -1,3 +1,26 @@
 <?php
-return array();
-?>
+return array (
+  'db' => 
+  array (
+    'host' => 'localhost',
+    'user' => 'root',
+    'pass' => 'root',
+    'schema' => 'cms',
+    'prefix' => '',
+  ),
+  'site' => 
+  array (
+    'deploy_checks' => true,
+    'upload_folder' => '/home/sque/workspace/cm5/uploads',
+    'cache_folder' => '/home/sque/workspace/cm5/cache',
+    'theme' => 'default',
+    'title' => 'test',
+    'timezone' => 'Africa/Abidjan',
+  ),
+  'module' => 
+  array (
+  ),
+  'email' => 
+  array (
+  ),
+);
--- themes/default/layout.php
+++ themes/default/layout.php
@@ -1,4 +1,25 @@
 <?php
+/*
+ *  This file is part of CM5 <http://code.0x0lab.org/p/cm5>.
+ *  
+ *  Copyright (c) 2010 Sque.
+ *  
+ *  CM5 is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published 
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  CM5 is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with CM5.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *  Contributors:
+ *      Sque - initial API and implementation
+ */
 
 class DefaultThemeLayout extends CM5_ThemeLayout
 {
